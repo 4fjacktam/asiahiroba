22b73445c467ae1b82418ebd2e1a3fdd8b34e9be
Jack Tam <4fjacktam@gmail.com>
2018-12-05 15:59:22 +0800

    Rename title

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Asia Hiroba'),
+      home: MyHomePage(title: 'Asia Hirobaa'),
     );
   }
 }
